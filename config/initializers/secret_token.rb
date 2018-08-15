0f02725b13d2ed17a3699147dd6037413071bf45
tim0414 <tim04142013@gmail.com>
2018-08-16 00:48:26 +0800

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Hw2::Application.config.secret_token = '3d8d7fcf5d59908cef5ba0e7155e009e9ef0c9827f372bde5ec3c3add810b024122a094d78361401bf1ce95e7d0c17e63ca4ce99f7b5bcea8ae6092d37dbfc84'
