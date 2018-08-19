de352a451a405844c2bc1f299d513b474710d194
tim0414 <tim04142013@gmail.com>
2018-08-20 02:16:54 +0800

    fixed model dependent relation

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -4,7 +4,7 @@ class Subscribe < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   belongs_to :subscriber, class_name: "User"
 
-  has_many :shares
+  has_many :shares, dependent: :delete_all
   has_many :cameras, through: :shares
   #has_one :owner, class_name: "User"
   #has_one :subscriber, class_name: "User"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
     attr_accessible :name, :email#, :password, :password_confirmation
 
     has_many :subscriber_subscribes, class_name: 'Subscribe', 
-             foreign_key: 'subscriber_id'
+             foreign_key: 'subscriber_id', dependent: :delete_all
     has_many :owner_subscribes, class_name: 'Subscribe', 
-             foreign_key: 'owner_id'
+             foreign_key: 'owner_id', dependent: :delete_all
 
-    has_many :cameras
+    has_many :cameras, dependent: :delete_all
 
 
     email_regex = /\A[\w+\-.]+@{a-z\d\-.}+\.[a-z]+\z/i
