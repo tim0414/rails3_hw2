c45a6ee546a2219fb837ceafd4b2face21295b68
tim0414 <tim04142013@gmail.com>
2018-08-20 02:30:46 +0800

    modify share, subscribe and camera dependency

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -5,7 +5,7 @@ class Subscribe < ActiveRecord::Base
   belongs_to :subscriber, class_name: "User"
 
   has_many :shares, dependent: :delete_all
-  has_many :cameras, through: :shares
+  has_many :cameras, through: :shares, dependent: :delete_all
   #has_one :owner, class_name: "User"
   #has_one :subscriber, class_name: "User"
 end
